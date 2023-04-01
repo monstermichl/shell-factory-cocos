4695997f60d206a7fff6080955931c7d97b6fbd9
monstermichl <vouillarmet.michel@gmail.com>
2023-04-01 19:58:11 +0200

    Add example

---
+++ examples/example.js
@@ -0,0 +1,17 @@
+import { Script } from 'shell-factory';
+import {
+    echo,
+    grep,
+    sed,
+} from '../dist/index.mjs';
+
+const script = new Script([
+    echo('Hello World'),
+
+    echo('Lets try to grep this command')
+        .pipe(grep('-e', 'this'))
+        .pipe(sed('-e', 's/this/that/g'))
+        .asRoot,
+]);
+
+console.log(script.dump());
